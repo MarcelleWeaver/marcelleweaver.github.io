df5175b332651353c752493e6f69eeb1a114a985
Marcelle <41974324+MichaelWeaver1@users.noreply.github.com>
2026-09-05 14:28:52 -0700

    now with my actual files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import './App.css';
 
-const App = () => {
-  return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
-    </div>
-  );
-};
+import { BrowserRouter, Routes, Route } from "react-router";
+import AboutMe from "./aboutMe";
+import Art from "./art";
+import Design from "./design";
+
+
+function App() {
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route  path="/" element={<Art />} />
+                    <Route  path="/art" element={<Art />} />
+
+                    <Route  path="/design" element={<Design />} />
+                    <Route  path="/aboutMe" element={<AboutMe />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+}
 
 export default App;
